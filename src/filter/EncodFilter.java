78b81d0644593977394b6506ee21f18281c79fb8
tf5488 <1174742234@qq.com>
2018-12-25 14:52:17 +0800

    修改架包error

--- src/filter/EncodFilter.java
+++ src/filter/EncodFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class EncodFilter implements Filter{
+public class EncodFilter implements filter{
 	private String charset;
 	
 	@Override
